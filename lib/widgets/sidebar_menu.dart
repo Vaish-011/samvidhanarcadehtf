218c3d01f96f4c3422b2abce564dacbfbe35b786
Rachita Laad <rachitalaad505@gmail.com>
2024-11-10 09:33:42 +0530

    done logout

--- lib/widgets/sidebar_menu.dart
+++ lib/widgets/sidebar_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../screens/feedback_form_screen.dart';
 import '../screens/bookmarked_summaries_screen.dart'; // Import BookmarkedSummariesScreen
+import '../screens/login_page.dart';
 
 class SidebarMenu extends StatelessWidget {
   @override
@@ -45,7 +46,7 @@ class SidebarMenu extends StatelessWidget {
               children: <Widget>[
                 _buildMenuItem(context, Icons.person, 'Profile'),
                 _buildMenuItem(context, Icons.bookmark, 'Bookmark', isBookmark: true), // Modify for Bookmark
-                _buildMenuItem(context, Icons.logout, 'Logout'),
+                _buildMenuItem(context, Icons.logout, 'Logout',isLogout: true),
                 _buildMenuItem(context, Icons.feedback, 'Feedback', isFeedback: true),
               ],
             ),
@@ -56,7 +57,7 @@ class SidebarMenu extends StatelessWidget {
   }
 
   // Helper function to build menu items
-  Widget _buildMenuItem(BuildContext context, IconData icon, String title, {bool isFeedback = false, bool isBookmark = false}) {
+  Widget _buildMenuItem(BuildContext context, IconData icon, String title, {bool isFeedback = false, bool isBookmark = false,bool isLogout = false}) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
       child: Card(
@@ -89,6 +90,13 @@ class SidebarMenu extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                   builder: (context) => BookmarkedSummariesScreen(), // Navigate to BookmarkedSummariesScreen
+                ),
+              );
+            }else if (isLogout) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LoginPage(), // Navigate to BookmarkedSummariesScreen
                 ),
               );
             } else {
